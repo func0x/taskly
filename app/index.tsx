45169d8f10bad78576f3d351b14add38af08f7df
Damian Janus <damian.janus@gmail.com>
2025-09-09 00:56:14 +0200

    add reac-native-get-random-value so that uuid works, added state for list and handleSubmit fn

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,48 @@
-import { StyleSheet, View } from "react-native";
+import "react-native-get-random-values";
+import { StyleSheet, TextInput, View } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-const shoppingItems = [
-  { name: "Coffee ☕️" },
-  { name: "Tea 🫖", isCompleted: true },
-  { name: "Milk 🥛" },
-  { name: "Cake 🍰" },
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+  isCompleted?: boolean;
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: uuidv4(), name: "Coffee ☕️" },
+  { id: uuidv4(), name: "Tea 🫖", isCompleted: true },
+  { id: uuidv4(), name: "Milk 🥛" },
+  { id: uuidv4(), name: "Cake 🍰" },
 ];
 
 export default function App() {
+  const [value, setValue] = useState("");
+  const [list, setList] = useState(initialList);
+
+  const handleSubmit = () => {
+    if (value) {
+      const newList = [...list, { id: uuidv4(), name: value }];
+      setList(newList);
+      setValue("");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      {shoppingItems.map((task, index) => {
+      <TextInput
+        placeholder="E.g. Coffee ☕️"
+        style={styles.textInput}
+        value={value}
+        onChangeText={setValue}
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+      />
+      {list.map(({ id, name, isCompleted }) => {
         return (
-          <ShoppingListItem
-            key={index}
-            name={task.name}
-            isCompleted={task.isCompleted}
-          />
+          <ShoppingListItem key={id} name={name} isCompleted={isCompleted} />
         );
       })}
     </View>
@@ -29,6 +53,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+    paddingTop: 12,
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: theme.colorLightGrey,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 6,
   },
 });
