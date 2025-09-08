1b6b1c7b3f719a6de42fb52c3519bbc9f2e3d4ae
Damian Janus <damian.janus@gmail.com>
2025-09-08 23:25:35 +0200

    install expo-router and other needed packages, create additional screens, use tab-navigation

---
+++ app/_layout.tsx
@@ -0,0 +1,38 @@
+import { Tabs } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import { theme } from "../theme";
+
+export default function Layout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping List",
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="clock" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ size, color }) => (
+            <FontAwesome6 name="lightbulb" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
