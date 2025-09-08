b58b2ea8bf6a47cd31253c540af8f148ec5412a0
Damian Janus <damian.janus@gmail.com>
2025-09-08 23:47:12 +0200

    add nested routing and new history screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,6 +22,7 @@ export default function Layout() {
           tabBarIcon: ({ size, color }) => (
             <Feather name="clock" size={size} color={color} />
           ),
+          headerShown: false,
         }}
       />
       <Tabs.Screen
